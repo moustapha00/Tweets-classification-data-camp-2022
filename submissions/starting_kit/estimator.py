7f2da130e28060a67951b3a10f166e274b961476
moustapha.mohamed-mahmoud <moustapha.mohamed-mahmoud@polytechnique.edu>
2023-02-18 17:42:02 +0100

    second commit

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -16,35 +16,12 @@ class Classifier(BaseEstimator):
         return y_pred
 
 
-def compute_rolling_std(X_df, feature, time_window, center=False):
-    """
-    For a given dataframe, compute the standard deviation over
-    a defined period of time (time_window) of a defined feature
-
-    Parameters
-    ----------
-    X : dataframe
-    feature : str
-        feature in the dataframe we wish to compute the rolling std from
-    time_window : str
-        string that defines the length of the time window passed to `rolling`
-    center : bool
-        boolean to indicate if the point of the dataframe considered is
-        center or end of the window
-    """
-    name = "_".join([feature, time_window, "std"])
-    X_df[name] = X_df[feature].rolling(time_window, center=center).std()
-    X_df[name] = X_df[name].ffill().bfill()
-    X_df[name] = X_df[name].astype(X_df[feature].dtype)
-    return X_df
-
-
 class FeatureExtractor(BaseEstimator):
     def fit(self, X, y):
         return self
 
     def transform(self, X):
-        return compute_rolling_std(X, "Beta", "2h")
+        return X
 
 
 def get_estimator():
